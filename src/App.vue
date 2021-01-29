4fb3c3f2d00f130cc1d89a5d876568d052f53c2d
yedarm <oncoder@naver.com>
2021-01-29 20:23:20 +0900

    네비게이션 분리

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-	<div id="nav">
-		<router-link to="/">부루마블</router-link>
-	</div>
+	<app-nav />
 	<router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppNav from './components/app-nav.vue';
+
+export default defineComponent({
+	name: 'App',
+
+	components: {
+		AppNav,
+	},
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -12,18 +23,5 @@
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-}
-
-#nav {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
 }
 </style>
