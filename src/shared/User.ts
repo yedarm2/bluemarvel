84d0aa2db6e285af56a013676336aa658d2c6b63
Jaewon-Noh <jwno@fastcampus.co.kr>
2021-02-16 21:24:58 +0900

    유저와 은행 클래스 생성

---
+++ src/shared/User.ts
@@ -0,0 +1,127 @@
+import { CityArea, SpecialArea, TradableAreaIdEnum, PriceInfo } from '@/shared/boardData';
+import { propertyType } from '@/shared/policy';
+
+type tradableArea = CityArea | SpecialArea;
+type propertySkeleton = {
+	tile: tradableArea,
+	properties: {
+		[property in propertyType]: number
+	}
+}
+type tileSkeleton = {
+	[id in TradableAreaIdEnum]?: propertySkeleton
+};
+
+export class User {
+	name: string;
+	remainedMoney: number = 0;
+	items: Array<any> = []; // TODO 현재는 any의 배열이나 추후 황금열쇠 객체가 될 듯.
+	tiles: tileSkeleton = {};
+
+	constructor(name: string) {
+		this.name = name;
+	}
+
+	setMoney(value: number) {
+		this.remainedMoney += value;
+	}
+
+	getMoney() {
+		return this.remainedMoney;
+	}
+
+	payMoney(value: number): number {
+		this.remainedMoney -= value;
+		return value;
+	}
+
+	setItems(item: any) {
+		this.items.push(item);
+	}
+
+	useItem(index: number) {
+		this.items.splice(index, 1);
+	}
+
+	setTiles(tile: tradableArea) {
+		this.tiles[tile.id] = {
+			tile,
+			properties: {
+				[propertyType.VILLA]: 0,
+				[propertyType.BUILDING]: 0,
+				[propertyType.HOTEL]: 0,
+			}
+		};
+	}
+
+	sellTiles(tile: tradableArea) {
+		delete this.tiles[tile.id];
+	}
+
+	setProperties(tile: tradableArea, property: propertyType) {
+		const selectedTile = this.tiles[tile.id];
+		if (selectedTile) {
+			selectedTile.properties[property]++;
+		}
+	}
+
+	getTotalPropertyPrice(tile: tradableArea): number {
+		const selectedTile = this.tiles[tile.id];
+		if (!selectedTile) return 0;
+		if (!(selectedTile.tile instanceof SpecialArea)) {
+			const priceOfVilla = selectedTile.properties.VILLA * selectedTile.tile.buildingPriceInfo.villaPrice;
+			const priceOfBuilding = selectedTile.properties.BUILDING * selectedTile.tile.buildingPriceInfo.buildingPrice;
+			const priceOfHotel = selectedTile.properties.HOTEL * selectedTile.tile.buildingPriceInfo.hotelPrice;
+			const totalPrice = priceOfVilla + priceOfBuilding + priceOfHotel;
+
+			return totalPrice === 0 ? selectedTile.tile.buildingPriceInfo.areaPrice : totalPrice;
+		} else {
+			return selectedTile.tile.price;
+		}
+	}
+
+	getSpecificPropertyPrice(tile: tradableArea, properties: Array<propertyType>): number {
+		const selectedTile = this.tiles[tile.id];
+		if (!selectedTile) return 0;
+		if (!(selectedTile.tile instanceof SpecialArea)) {
+			if (properties.length > 0) {
+				return properties.reduce((prev, curr) => {
+					switch (curr) {
+						case propertyType.VILLA:
+							prev += selectedTile.tile instanceof CityArea ? selectedTile.tile.buildingPriceInfo.villaPrice : 0;
+							break;
+
+						case propertyType.BUILDING:
+							prev += selectedTile.tile instanceof CityArea ? selectedTile.tile.buildingPriceInfo.buildingPrice : 0;
+							break;
+
+						case propertyType.HOTEL:
+							prev += selectedTile.tile instanceof CityArea ? selectedTile.tile.buildingPriceInfo.hotelPrice : 0;
+							break;
+					}
+					return prev;
+				}, 0);
+			} else {
+				return selectedTile.tile.buildingPriceInfo.areaPrice
+			}
+		} else {
+			return selectedTile.tile.price;
+		}
+	}
+
+	getTolls(tile: tradableArea): number {
+		const selectedTile = this.tiles[tile.id];
+		if (!selectedTile) return 0;
+		if (!(selectedTile.tile instanceof SpecialArea)) {
+			const priceOfVilla = selectedTile.properties.VILLA * selectedTile.tile.paymentInfo.villaPrice;
+			const priceOfBuilding = selectedTile.properties.BUILDING * selectedTile.tile.paymentInfo.buildingPrice;
+			const priceOfHotel = selectedTile.properties.HOTEL * selectedTile.tile.paymentInfo.hotelPrice;
+			const totalPrice = priceOfVilla + priceOfBuilding + priceOfHotel;
+
+			return totalPrice === 0 ? selectedTile.tile.paymentInfo.areaPrice : totalPrice;
+		} else {
+			return selectedTile.tile.payment;
+		}
+	}
+
+}
