13ba2db49b016f37be124dd42f8f59a2e4aa29a0
Jaewon-Noh <jwno@fastcampus.co.kr>
2021-02-22 22:57:49 +0900

    타일 관련 함수들 제거

--- src/shared/User.ts
+++ src/shared/User.ts
@@ -1,16 +1,16 @@
-import { CityArea, SpecialArea, TradableAreaIdEnum } from '@/shared/boardData';
-import { propertyType } from '@/shared/policy';
+// import { CityArea, SpecialArea, TradableAreaIdEnum } from '@/shared/boardData';
+// import { propertyType } from '@/shared/policy';
 
-type tradableArea = CityArea | SpecialArea;
-type propertySkeleton = {
-	tile: tradableArea;
-	properties: {
-		[property in propertyType]: number
-	};
-}
-type tileSkeleton = {
-	[id in TradableAreaIdEnum]?: propertySkeleton;
-};
+// type tradableArea = CityArea | SpecialArea;
+// type propertySkeleton = {
+// 	tile: tradableArea;
+// 	properties: {
+// 		[property in propertyType]: number
+// 	};
+// }
+// type tileSkeleton = {
+// 	[id in TradableAreaIdEnum]?: propertySkeleton;
+// };
 
 export class User {
 	id: number;
@@ -19,7 +19,7 @@ export class User {
 
 	items = []; // TODO 현재는 any의 배열이나 추후 황금열쇠 객체가 될 듯.
 
-	tiles: tileSkeleton = {};
+	// tiles: tileSkeleton = {};
 
 	constructor(id: number) {
 		this.id = id;
@@ -45,86 +45,4 @@ export class User {
 	useItem(index: number) {
 		this.items.splice(index, 1);
 	}
-
-	setTiles(tile: tradableArea) {
-		this.tiles[tile.id] = {
-			tile,
-			properties: {
-				[propertyType.VILLA]: 0,
-				[propertyType.BUILDING]: 0,
-				[propertyType.HOTEL]: 0,
-			}
-		};
-	}
-
-	sellTiles(tile: tradableArea) {
-		delete this.tiles[tile.id];
-	}
-
-	setProperties(tile: tradableArea, property: propertyType) {
-		const selectedTile = this.tiles[tile.id];
-		if (selectedTile) {
-			selectedTile.properties[property] += 1;
-		}
-	}
-
-	getTotalPropertyPrice(tile: tradableArea): number {
-		const selectedTile = this.tiles[tile.id];
-		if (!selectedTile) return 0;
-		if (!(selectedTile.tile instanceof SpecialArea)) {
-			const priceOfVilla = selectedTile.properties.VILLA * selectedTile.tile.buildingPriceInfo.villaPrice;
-			const priceOfBuilding = selectedTile.properties.BUILDING * selectedTile.tile.buildingPriceInfo.buildingPrice;
-			const priceOfHotel = selectedTile.properties.HOTEL * selectedTile.tile.buildingPriceInfo.hotelPrice;
-			const totalPrice = priceOfVilla + priceOfBuilding + priceOfHotel + selectedTile.tile.buildingPriceInfo.areaPrice;
-
-			return totalPrice === 0 ? selectedTile.tile.buildingPriceInfo.areaPrice : totalPrice;
-		}
-		return selectedTile.tile.price;
-	}
-
-	getSpecificPropertyPrice(tile: tradableArea, properties: Array<propertyType>): number {
-		const selectedTile = this.tiles[tile.id];
-		if (!selectedTile) return 0;
-		if (!(selectedTile.tile instanceof SpecialArea)) {
-			if (properties.length > 0) {
-				/* eslint-disable no-param-reassign */
-				return properties.reduce((prev, curr) => {
-					switch (curr) {
-						case propertyType.VILLA:
-							prev += selectedTile.tile instanceof CityArea ? selectedTile.tile.buildingPriceInfo.villaPrice : 0;
-							break;
-
-						case propertyType.BUILDING:
-							prev += selectedTile.tile instanceof CityArea ? selectedTile.tile.buildingPriceInfo.buildingPrice : 0;
-							break;
-
-						case propertyType.HOTEL:
-							prev += selectedTile.tile instanceof CityArea ? selectedTile.tile.buildingPriceInfo.hotelPrice : 0;
-							break;
-						default:
-							break;
-					}
-					return prev;
-				}, 0);
-				/* eslint-enable no-param-reassign */
-			}
-			return selectedTile.tile.buildingPriceInfo.areaPrice;
-		}
-		return selectedTile.tile.price;
-	}
-
-	getTolls(tile: tradableArea): number {
-		const selectedTile = this.tiles[tile.id];
-		if (!selectedTile) return 0;
-		if (!(selectedTile.tile instanceof SpecialArea)) {
-			const priceOfVilla = selectedTile.properties.VILLA * selectedTile.tile.paymentInfo.villaPrice;
-			const priceOfBuilding = selectedTile.properties.BUILDING * selectedTile.tile.paymentInfo.buildingPrice;
-			const priceOfHotel = selectedTile.properties.HOTEL * selectedTile.tile.paymentInfo.hotelPrice;
-			const totalPrice = priceOfVilla + priceOfBuilding + priceOfHotel;
-
-			return totalPrice === 0 ? selectedTile.tile.paymentInfo.areaPrice : totalPrice;
-		}
-		return selectedTile.tile.payment;
-	}
-
 }
