e76791a1a1cefb4ed4c21e2d940582af2ac493d4
yedarm <oncoder@naver.com>
2021-02-23 23:52:21 +0900

    모서리를 제외한 tile의 template 컴포넌트 및 user pin을 보여주는 컴포넌트 추가

---
+++ src/components/board/board-tile-template.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="board-tile-template">
+		<slot />
+		<player-pin-displayer :users="users" />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import PlayerPinDisplayer from './player-pin-displayer.vue';
+
+export default defineComponent({
+	name: 'board-tile-template',
+
+	components: {
+		PlayerPinDisplayer,
+	},
+
+	props: {
+		users: {
+			type: Array,
+			default: () => [],
+		},
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+@import './variables.scss';
+
+.board-tile-template {
+	@include tile-style(false);
+}
+</style>
---
+++ src/components/board/player-pin-displayer.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="player-pin-displayer">
+		<div v-for="user in users" :key="user.id" class="player-pin-displayer__pin">
+			<div class="player-pin-displayer__pin-body" />
+			<div class="player-pin-displayer__pin-head" />
+			<div class="player-pin-displayer__pin-id-text">
+				{{ user.id }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'player-pin-displayer',
+
+	props: {
+		users: {
+			type: Array,
+			default: () => [],
+		},
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+$pin-side-border-value: 5px solid transparent;
+
+.player-pin-displayer {
+	display: flex;
+	justify-content: center;
+
+	width: 100%;
+
+	position: absolute;
+	bottom: calc(100% + 10px);
+
+	&__pin {
+		position: relative;
+		width: 20px;
+		height: 40px;
+	}
+
+	&__pin-head {
+		width: 15px;
+		height: 15px;
+
+		border-radius: 100px;
+
+		position: absolute;
+		left: 50%;
+		top: 0;
+		transform: translate(-50%);
+
+		background-color: gold;
+	}
+
+	&__pin-body {
+		width: 7px;
+		border-bottom: 30px solid gold;
+		border-left: $pin-side-border-value;
+		border-right: $pin-side-border-value;
+
+		position: absolute;
+		left: 50%;
+		bottom: 0;
+		transform: translate(-50%);
+	}
+
+	&__pin-id-text {
+		position: absolute;
+		left: 50%;
+		bottom: 2px;
+		transform: translate(-50%);
+	}
+}
+</style>
