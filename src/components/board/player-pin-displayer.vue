f22e395b7af9c209d4579907c91a6893ed4c3c2e
yedarm <oncoder@naver.com>
2021-02-24 00:00:15 +0900

    pin을 컴포넌트로 분리

--- src/components/board/player-pin-displayer.vue
+++ src/components/board/player-pin-displayer.vue
@@ -1,20 +1,20 @@
 <template>
 	<div class="player-pin-displayer">
-		<div v-for="user in users" :key="user.id" class="player-pin-displayer__pin">
-			<div class="player-pin-displayer__pin-body" />
-			<div class="player-pin-displayer__pin-head" />
-			<div class="player-pin-displayer__pin-id-text">
-				{{ user.id }}
-			</div>
-		</div>
+		<player-pin v-for="user in users" :key="user.id" :id="user.id" />
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import PlayerPin from './player-pin.vue';
+
 export default defineComponent({
 	name: 'player-pin-displayer',
+
+	components: {
+		PlayerPin,
+	},
 
 	props: {
 		users: {
@@ -26,8 +26,6 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-$pin-side-border-value: 5px solid transparent;
-
 .player-pin-displayer {
 	display: flex;
 	justify-content: center;
@@ -41,39 +39,6 @@ $pin-side-border-value: 5px solid transparent;
 		position: relative;
 		width: 20px;
 		height: 40px;
-	}
-
-	&__pin-head {
-		width: 15px;
-		height: 15px;
-
-		border-radius: 100px;
-
-		position: absolute;
-		left: 50%;
-		top: 0;
-		transform: translate(-50%);
-
-		background-color: gold;
-	}
-
-	&__pin-body {
-		width: 7px;
-		border-bottom: 30px solid gold;
-		border-left: $pin-side-border-value;
-		border-right: $pin-side-border-value;
-
-		position: absolute;
-		left: 50%;
-		bottom: 0;
-		transform: translate(-50%);
-	}
-
-	&__pin-id-text {
-		position: absolute;
-		left: 50%;
-		bottom: 2px;
-		transform: translate(-50%);
 	}
 }
 </style>
---
+++ src/components/board/player-pin.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="player-pin">
+		<div class="player-pin__body" />
+		<div class="player-pin__head" />
+		<div class="player-pin__id-text">
+			{{ id }}
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'player-pin',
+
+	props: {
+		id: {
+			type: Number,
+			required: true,
+		},
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+$pin-side-border-value: 5px solid transparent;
+
+.player-pin {
+	position: relative;
+	width: 20px;
+	height: 40px;
+
+	&__head {
+		width: 15px;
+		height: 15px;
+
+		border-radius: 100px;
+
+		position: absolute;
+		left: 50%;
+		top: 0;
+		transform: translate(-50%);
+
+		background-color: gold;
+	}
+
+	&__body {
+		width: 7px;
+		border-bottom: 30px solid gold;
+		border-left: $pin-side-border-value;
+		border-right: $pin-side-border-value;
+
+		position: absolute;
+		left: 50%;
+		bottom: 0;
+		transform: translate(-50%);
+	}
+
+	&__id-text {
+		position: absolute;
+		left: 50%;
+		bottom: 2px;
+		transform: translate(-50%);
+	}
+}
+</style>
