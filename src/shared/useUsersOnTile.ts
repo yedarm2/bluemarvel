e22a0ea65f12169d0b424b7633c203b35946f1ee
yedarm <oncoder@naver.com>
2021-02-24 16:57:43 +0900

    임시 코드에서 vuex 바인딩으로 변경

--- src/shared/useUsersOnTile.ts
+++ src/shared/useUsersOnTile.ts
@@ -12,29 +12,30 @@ interface User {
 
 export default (currentTile: Ref<BaseTile>) => {
 	const store = useStore();
-	// const users: Ref<User[]> = computed(() => store.state.gameInterface.users);
-	const users: Ref<User[]> = computed(() => [
-		{
-			id: 1,
-			currentPosition: tileList[0],
-		},
-		{
-			id: 2,
-			currentPosition: tileList[0],
-		},
-		{
-			id: 3,
-			currentPosition: tileList[9],
-		},
-		{
-			id: 4,
-			currentPosition: tileList[20],
-		},
-	]);
+	const users: Ref<User[]> = computed(() => store.state.gameInterface.users);
+	// const users: Ref<User[]> = computed(() => [
+	// 	{
+	// 		id: 1,
+	// 		currentPosition: tileList[0],
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		currentPosition: tileList[0],
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		currentPosition: tileList[9],
+	// 	},
+	// 	{
+	// 		id: 4,
+	// 		currentPosition: tileList[20],
+	// 	},
+	// ]);
 
 	return computed(() =>
 		users.value.filter(({ currentPosition }) =>
-			currentPosition.compareTile(currentTile.value),
+			// TODO: User interface는 임시용이어서 optional chaining 사용
+			currentPosition?.compareTile(currentTile.value),
 		),
 	);
 };
