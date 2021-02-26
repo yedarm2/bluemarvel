d7a8c8f4d009a3b7eb2db7ee122b38037aca5b94
yedarm <oncoder@naver.com>
2021-02-27 00:26:00 +0900

    useUsersOnTile을 user와 연동

--- src/shared/useUsersOnTile.ts
+++ src/shared/useUsersOnTile.ts
@@ -2,40 +2,18 @@ import { computed, Ref } from 'vue';
 import { useStore } from 'vuex';
 
 import { BaseTile, tileMatrix } from './boardData';
+import { User } from './User';
 
 const tileList = tileMatrix.flat();
-
-interface User {
-	id: number;
-	currentPosition: BaseTile;
-}
 
 export default (currentTile: Ref<BaseTile>) => {
 	const store = useStore();
 	const users: Ref<User[]> = computed(() => store.state.gameInterface.users);
-	// const users: Ref<User[]> = computed(() => [
-	// 	{
-	// 		id: 1,
-	// 		currentPosition: tileList[0],
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		currentPosition: tileList[0],
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		currentPosition: tileList[9],
-	// 	},
-	// 	{
-	// 		id: 4,
-	// 		currentPosition: tileList[20],
-	// 	},
-	// ]);
 
 	return computed(() =>
-		users.value.filter(({ currentPosition }) =>
+		users.value.filter(({ currentPositionTile }) =>
 			// TODO: User interface는 임시용이어서 optional chaining 사용
-			currentPosition?.compareTile(currentTile.value),
+			currentPositionTile.compareTile(currentTile.value),
 		),
 	);
 };
