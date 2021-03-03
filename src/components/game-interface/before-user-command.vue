d50c89f517786592bac5e91ede253ab94eedb07f
Jaewon-Noh <jwno@fastcampus.co.kr>
2021-03-03 20:23:59 +0900

    사용하지 않는 매개변수 제거.

--- src/components/game-interface/before-user-command.vue
+++ src/components/game-interface/before-user-command.vue
@@ -56,7 +56,7 @@ export default defineComponent({
 		}
 	},
 	emits: ['hide-game-interface', 'trade-with-bank'],
-	setup(_, { emit }) {
+	setup() {
 		return {
 			...useBoardContext(),
 			rollDice: useRollDice(),
