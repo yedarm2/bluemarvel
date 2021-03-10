f4cec3db9f20b241d5e59262b1cbc9a7fdf099e7
yedarm <oncoder@naver.com>
2021-03-10 18:49:10 +0900

    황금 열쇠 관련 컴포넌트 추가

---
+++ src/components/game-interface/golden-key-processor.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="golden-key-processor">
+		황금열쇠를 뽑아야 합니다.
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'GoldenKeyProcessor',
+
+	setup() {
+		return {};
+	},
+})
+</script>
+
