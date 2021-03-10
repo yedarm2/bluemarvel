abd595d616a628872b8c76efff56a5ec1b771e41
yedarm <oncoder@naver.com>
2021-03-10 20:52:08 +0900

    황금열쇠 컴포넌트에 관련 로직 추가

--- src/components/game-interface/golden-key-processor.vue
+++ src/components/game-interface/golden-key-processor.vue
@@ -1,18 +1,55 @@
 <template>
 	<div class="golden-key-processor">
-		황금열쇠를 뽑아야 합니다.
+		<tempalte v-if="drawedGoldenKey === null">
+			황금열쇠를 뽑고 있습니다.
+		</tempalte>
+		<template v-else>
+			<div class="golden-key-processor__golden-key-title">
+				{{ drawedGoldenKey.title }}
+			</div>
+			<div class="golden-key-processor__golden-key-description">
+				{{ drawedGoldenKey.description }}
+			</div>
+		</template>
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, onUnmounted } from 'vue';
+import { useStore } from 'vuex';
 
 export default defineComponent({
 	name: 'GoldenKeyProcessor',
 
 	setup() {
-		return {};
+		const {
+			state,
+			commit,
+			dispatch,
+		} = useStore();
+
+		const drawedGoldenKey = computed(() => state.gameInterface.goldenKey.drawedGoldenKey);
+
+		dispatch('gameInterface/goldenKey/drawGoldenKey');
+
+		onUnmounted(() => {
+			commit('gameInterface/goldenKey/removeGoldenKey');
+		});
+
+		return {
+			drawedGoldenKey,
+		};
 	},
 })
 </script>
 
+<style lang="scss" scoped>
+.golden-key-processor {
+	text-align: center;
+
+	&__golden-key-title {
+		font-size: 1.2em;
+		font-weight: bold;
+	}
+}
+</style>
